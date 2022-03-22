c2bdd6139d34e413fe643c164b0a2a8daba37b34
Halkrine <halkrine@gmail.com>
2022-03-22 11:15:34 +0900

    Creata Parse Util Mockup

--- src/main/java/com/woodvill/isohonaru/foodgraph/FoodGraphRestController.java
+++ src/main/java/com/woodvill/isohonaru/foodgraph/FoodGraphRestController.java
@@ -3,12 +3,30 @@ package com.woodvill.isohonaru.foodgraph;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class FoodGraphRestController {
 
 
+    /**
+     * mockup crud test
+     * @return
+     */
     @RequestMapping(value="/isohonaru/foodRest")
     public String foodRest(){
         return "connected!!";
     }
+
+    @RequestMapping(value="/isohonaru/foodRest/getList")
+    public List getList(HttpServletRequest req, HttpServletResponse res){
+        List rtnList = new ArrayList();
+
+        // get list from gdb
+        return rtnList;
+    }
+
 }
